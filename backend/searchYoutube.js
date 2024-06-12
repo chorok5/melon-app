d3d6a127ff9b89cbca9ba7fac53f2838d6e7e5d9
Chorok Oh <157563710+chorok5@users.noreply.github.com>
2024-06-12 21:55:45 +0900

    Update searchYoutube.js

--- backend/searchYoutube.js
+++ backend/searchYoutube.js
@@ -29,35 +29,3 @@ app.get('/api/search', async (req, res) => {
   }
 });
 
-
-
-
-// 곡 리스트를 받아서 각 곡에 대한 유튜브 링크를 생성하는 함수
-// const generateYoutubeLinks = async (playlist) => {
-//   const youtubeLinks = [];
-
-//   for (const song of playlist) {
-//     const query = `${song.title} ${song.artist} ${song.album}`;
-//     const youtubeLink = await searchYoutube(query);
-//     if (youtubeLink) {
-//       youtubeLinks.push(youtubeLink);
-//     }
-//   }
-
-//   return youtubeLinks;
-// };
-
-// // 예시 플레이리스트
-// const playlist = [
-//   { title: 'Song Title 1', artist: 'Artist 1', album: 'Album 1' },
-//   { title: 'Song Title 2', artist: 'Artist 2', album: 'Album 2' },
-//   // 추가적인 곡 정보들...
-// ];
-
-// // 유튜브 링크 생성
-// generateYoutubeLinks(playlist)
-//   .then((youtubeLinks) => {
-//     console.log('Youtube Links:', youtubeLinks);
-//     // 이제 유튜브 링크를 사용하여 재생목록을 만들거나 출력할 수 있음
-//   })
-//   .catch((error) => console.error('Error generating YouTube links:', error));
